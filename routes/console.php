e28007c40412de709d9b5521c81c88137f336eee
asmeyb <asmeyb@gmail.com>
2025-11-12 16:23:41 +0300

    Command for recurring expenses

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,13 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('expense:generate-recurring-expense')->everyFifteenSeconds()
+    ->withoutOverlapping()
+    ->onSuccess(function () {
+        // Actions to perform on success
+        Log::info('Successfully executed expense:generate-recurring-expense command.');
+    })->onFailure(function () {
+        // Actions to perform on failure
+        Log::error('Failed to execute expense:generate-recurring-expense command.');
+    });
